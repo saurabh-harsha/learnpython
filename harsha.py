18761def9a8bda8bd21789b2b825d32f8bbc16be
saurabh-harsha <harshabhagwani3@gmil.com>
2024-03-05 00:02:34 +0530

    one function added

--- harsha.py
+++ harsha.py
@@ -1,3 +1,10 @@
 print("hello")
 print("this is  my python file ")
 print("hey yes")
+def sum():
+    a=int(input("enter the frst number"))
+    b=int(input('enter the second number'))
+    c=a+b
+    return (c)
+sum1=sum()
+print("the addition of two number:",sum1)
